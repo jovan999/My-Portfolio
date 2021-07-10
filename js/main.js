1c4d39fb8fcda5fac861a8ad909df77486e3097e
Jovan Smiljanic <56159778+jovan999@users.noreply.github.com>
2021-07-10 02:33:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -202,7 +202,7 @@ function render() {
 }
 
 new TypeIt(".multipleStrings", {
-  strings: ["Hi, I am Jovan Smiljanic,", "Front-end Developer."],
+  strings: ["Hello, My name is Jovan,", "I am Frontend Developer."],
   speed: 80,
   waitUntilVisible: true,
   loop: true,
